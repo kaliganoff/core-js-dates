9d02c422f8bc1fe4befe0f50ca1d5e0ba27d0ead
kaliganoff <kaliganoff@yahoo.com>
2024-03-25 01:39:09 +0300

    feat: Solve two tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -217,8 +217,8 @@ function getNextFridayThe13th(/* date */) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  return Math.ceil((date.getMonth() + 1) / 3);
 }
 
 /**
@@ -255,8 +255,8 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  return date.getFullYear() % 4 === 0;
 }
 
 module.exports = {
